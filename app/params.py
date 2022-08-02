d986f9d115d6cc2bd69c39cd6fe890f7fbe46352
camipozas <cpozasg1103@gmail.com>
2022-08-01 22:21:43 -0400

    update params, delete chromedriver

---
+++ app/params.py
@@ -0,0 +1,20 @@
+import os
+from environs import Env
+
+# Credenciales
+env = Env()
+env.read_env()  # read .env file, if it exists
+
+# SAP
+user_name = env("user_name")
+password = env("password")
+
+# SQL
+server_sql = env("server_sql")
+database = env("database")
+user_sql = env("user_sql")
+pass_sql = env("pass_sql")
+mssql_driver = env("mssql_driver", "ODBC+Driver+17+for+SQL+Server")
+
+# Output
+output_dir = os.path.join(os.getcwd(), 'output')
